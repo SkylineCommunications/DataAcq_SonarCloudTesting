0deeaa10acd13f227be46b917855283b278adf61
Michiel Oda <michiel.oda@skyline.be>
2023-11-13 11:19:13 +0100

    no message

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTestProject1
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
